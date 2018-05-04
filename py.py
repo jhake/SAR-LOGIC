360bac2ea2a5dc73ab8f561d5558c92b301d0e1a
jhakezebedee <jhake.aquino123@gmail.com>
2018-05-04 22:21:38 +0800

    with k6 and power trials
    with k6 and power trials

--- py.py
+++ py.py
@@ -1,6 +1,6 @@
 read = open("dontuse.txt","r")
 write = open("dontuse2.txt","w")
-data = read.read().replace('\r\n',' ')
+data = read.read().replace('\r\n',' ').replace('HS65','CORE65LPHVT/HS65')
 
 write.write(data)
 
